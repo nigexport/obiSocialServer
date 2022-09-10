e410824bae7d853d657abf72584a60ff8f273e0c
nigexport <rhassan_owolabi@yahoo.com>
2022-09-10 23:18:54 +0100

    updated

--- index.js
+++ index.js
@@ -2,33 +2,32 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import AuthRoute from './Routes/AuthRoute.js'
-import UserRoute from './Routes/UserRoute.js'
-import PostRoute from './Routes/PostRoute.js'
+import cors from "cors";
+import AuthRoute from "./Routes/AuthRoute.js";
+import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js";
 
 //Routes
 
 const app = express();
 
 //Middleware
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
-
-dotenv.config()
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO_DB, {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    })
-    .then(()=>
-        app.listen(process.env.PORT, ()=>
-            console.log("Listening at port 5000")
-        )
-    )
-    .catch((error) => console.log(error));
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () => console.log("Listening at port 5000"))
+  )
+  .catch((error) => console.log(error));
 
-    //usage of routes
-    app.use('/auth', AuthRoute)
-    app.use('/user', UserRoute)
-    app.use('/post', PostRoute)
+//usage of routes
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use("/post", PostRoute);
