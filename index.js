f904be9ca83498cd4770e6dc349733b0d8091fca
nigexport <rhassan_owolabi@yahoo.com>
2022-09-11 20:24:07 +0100

    updated

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import AuthRoute from "./Routes/AuthRoute.js";
-import UserRoute from "./Routes/UserRoute.js";
-import PostRoute from "./Routes/PostRoute.js";
-
+import AuthRoute from './Routes/AuthRoute.js'
+import UserRoute from './Routes/UserRoute.js'
+import PostRoute from './Routes/PostRoute.js'
+import UploadRoute from './Routes/UploadRoute.js'
 //Routes
 
 const app = express();
@@ -31,3 +31,4 @@ mongoose
 app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
 app.use("/post", PostRoute);
+app.use('/upload', UploadRoute)
